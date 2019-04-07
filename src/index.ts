2e7cbba58b51d1cf57f9d01fb357ba7e82d79f0b
maudnals <maud.nalpas@gmail.com>
2019-04-07 08:52:53 +0200

    feat(index): added title

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,23 @@ const tokens = tokenize(input);
 const ast = parse(tokens);
 
 console.table(tokens);
-console.log(ast);
+console.log(JSON.stringify(ast));
+
+const ast2 = {
+  type: 2,
+  body: [
+    {
+      name: 'add',
+      type: 1,
+      params: [{ type: 3, value: 'a' }, { type: 3, value: 'b' }],
+      returnValue: {
+        type: 0,
+        value: {
+          type: 4,
+          operator: '+',
+          operands: [{ type: 3, value: 'a' }, { type: 3, value: 'b' }]
+        }
+      }
+    }
+  ]
+};
